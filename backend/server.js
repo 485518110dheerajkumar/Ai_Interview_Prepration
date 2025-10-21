83d11c595787cc40b1f58ff584f28ebac391cc07
pk485518110pk <pk485518110pk@gmail.com>
2025-10-21 13:16:36 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -94,7 +94,9 @@ app.post("/api/contact", async (req, res) => {
 
 
 // ✅ Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`✅ Server running on http://localhost:${PORT}`);
+// });
+
+module.exports = app;
