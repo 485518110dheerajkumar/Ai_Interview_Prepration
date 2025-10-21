90f102caca5ef1960aa8308ca709dcfe276530ec
pk485518110pk <pk485518110pk@gmail.com>
2025-10-21 15:47:36 +0530

    eight commit

--- backend/server.js
+++ backend/server.js
@@ -93,4 +93,7 @@ app.post("/api/contact", async (req, res) => {
 
 
 // ✅ Start server
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
